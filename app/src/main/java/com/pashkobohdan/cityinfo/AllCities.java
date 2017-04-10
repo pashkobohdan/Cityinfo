ce34ec05b7521104688ef194b5a426d53ced4b2e
pashkobohdan <pashkobohdan@gmail.com>
2017-04-10 21:20:41 +0300

    Initial commit

---
+++ app/src/main/java/com/pashkobohdan/cityinfo/AllCities.java
@@ -0,0 +1,13 @@
+package com.pashkobohdan.cityinfo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AllCities extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_cities);
+    }
+}
